b52c4bcffbdc5650b2414b83a63633d8ea4cbec7
dilaafkar-dotcom <dilaafkar@gmail.com>
2026-05-23 10:50:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ st.set_page_config(
     page_title="Kuliah_Praktisi_230526",
     page_icon-"🧊",
     layout="centered",
-    initial_sidebar_state="expanted"
-                                     )
+    initial_sidebar_state="expanded)
 
 # Hirarki teks
 st.title("📊 Dashboard")
